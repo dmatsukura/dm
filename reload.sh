e870ed93baf87febbb24dd6c1c8a87daaa2e8a11
dmatsukura <dicekma@gmail.com>
2023-10-16 12:27:30 -0400

    set up concluded

--- reload.sh
+++ reload.sh
@@ -3,11 +3,11 @@
 ./venv/bin/python3 manage.py check --deploy
 ./venv/bin/python3 manage.py collectstatic --noinput
 ./venv/bin/python3 manage.py migrate
-touch dm/wsgi.py
+touch dm_django/wsgi.py
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
-#systemctl restart celery
-#systemctl restart celery-beat
+systemctl restart celery
+systemctl restart celery-beat
